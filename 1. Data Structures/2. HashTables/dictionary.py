447d0eac265ed0c2862ab8d7c8231f73d717463e
kamil-1929 <kamil.329@outlook.com>
2025-03-07 17:03:53 +0000

    hash

---
+++ 1. Data Structures/2. HashTables/dictionary.py
@@ -0,0 +1,11 @@
+
+    
+
+# Function to check if the current environment is Google Colab
+def is_google_colab():
+    try:
+        import google.colab
+        return True
+    except:
+        return False
+    
