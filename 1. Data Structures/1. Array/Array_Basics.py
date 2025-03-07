ee77e141b2bb66dfdb92b3203aebf815b3003b8c
kamil-1929 <kamil.329@outlook.com>
2025-03-07 17:02:11 +0000

    array

---
+++ 1. Data Structures/1. Array/Array_Basics.py
@@ -0,0 +1,17 @@
+# Create a new List
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+#Accessing the elements of an array
+first_element = numbers[0]
+last_element = numbers[-1]
+
+# Modifying the elements of an array
+numbers[0] = 10 # Change 1 to 10
+numbers[-1] = 1 # Change 10 to 1
+print(numbers[0]) # Output: 10
+
+#Appending a new element
+numbers.append(11)
+print(numbers)
+
+# Appending a new
