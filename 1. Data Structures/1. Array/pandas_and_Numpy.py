9b0166be729441dde94d93b7b4e4436840cb3942
kamil-1929 <kamil.329@outlook.com>
2025-03-10 16:56:02 +0000

    Advanced Arrays

---
+++ 1. Data Structures/1. Array/pandas_and_Numpy.py
@@ -0,0 +1,16 @@
+# Practicing pandas and numpy arrays for data manipulation purposes only for now
+# as it helps performance and ease of use for data manipulation
+# Pandas is a library that provides data structures and data analysis tools for Python programming language
+# and data manipulation tools for data analysis tasks.
+# it helps in Data structures like Series and DataFrame objects that are built on top of NumPy.
+
+# Using numpy for numerical operations
+import numpy as np
+import pandas as pd
+
+# Creating a sample dataframe
+numbers = [1, 2, 3, 4, 5]
+array = np.array(numbers)
+print(array)
+mean = np.mean(array)
+print(mean)
